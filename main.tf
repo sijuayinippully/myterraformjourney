3d5c48b036de3eeda84b0d8f9ed0492165e9a71f
sijuayinippully <86511984+sijuayinippully@users.noreply.github.com>
2024-03-06 13:44:00 +0400

    commit-10

--- main.tf
+++ main.tf
@@ -17,7 +17,16 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
-
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.1"
+  name = "blog_new"
+  vpc_id = data.aws_vpc.default.id
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
 resource "aws_security_group" "blog"{
     name = "blog"
     description = "Allow http and https in. Allow everything out" 
@@ -27,7 +36,7 @@ resource "aws_security_group" "blog"{
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "Learning_Terrform_VM"
   }
