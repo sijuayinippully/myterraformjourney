1333e8c2775434fbe19c3155270ad7389099548b
sijuayinippully <86511984+sijuayinippully@users.noreply.github.com>
2024-03-06 15:06:55 +0400

    autoscalinggroup

--- main.tf
+++ main.tf
@@ -49,17 +49,31 @@ resource "aws_security_group" "blog"{
     vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+#resource "aws_instance" "blog" {
+  #ami           = data.aws_ami.app_ami.id
+  #instance_type = var.instance_type
+  #vpc_security_group_ids = [module.blog_sg.security_group_id]
+  #subnet_id = module.blog_vpc.public_subnets[0]
+  #tags = {
+  #  Name = "Learning_Terrform_VM"
+  #}
+#}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.0"
+  name = "blog-asg"
+  min_size = 1
+  max_size = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  image_id = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
-  tags = {
-    Name = "Learning_Terrform_VM"
-  }
+  # insert the 1 required variable here
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
