8fcaddf8f30341f71b8c40b4f23d34880b9fd202
sijuayinippully <86511984+sijuayinippully@users.noreply.github.com>
2024-03-06 14:48:33 +0400

    loadbalancer-3

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ module "alb" {
 
   name    = "blog-alb"
   vpc_id  = "module.blog_vpc.vpc_id"
-  subnets = "module.blog_vpc.public_subnets"
+  subnets = ["module.blog_vpc.public_subnets"]
   security_groups = ["module.blog_sg.security_group_id"]
  
   target_groups = {
