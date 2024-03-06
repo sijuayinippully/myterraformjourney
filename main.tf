05cfdb3131bc77bd90ffb930963976c561afa638
sijuayinippully <86511984+sijuayinippully@users.noreply.github.com>
2024-03-06 12:01:59 +0400

    commit

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group_rule" "blog_http_in"{
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = aws.blog.id
+    security_group_id = aws_security_group.blog.id
 }
 resource "aws_security_group_rule" "blog_https_in"{
     type = "ingress"
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "blog_https_in"{
     to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = aws.blog.id
+    security_group_id = aws_security_group.blog.id
 }
 resource "aws_security_group_rule" "blog_everything_out"{
     type = "egress"
@@ -54,5 +54,5 @@ resource "aws_security_group_rule" "blog_everything_out"{
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = aws.blog.id
+    security_group_id = aws_security_group.blog.id
 }
